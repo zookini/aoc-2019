e117add42ee3ccb3551e762765a75a356ee7dfba
Prefectly Cromulent <prefectly.cromulent@gmail.com>
2020-01-03 16:18:40 +0000

    Use shared Intcode computer for days 17-19

--- src/bin/q17-2.rs
+++ src/bin/q17-2.rs
@@ -1,20 +1,25 @@
 use aoc::*;
+use futures::prelude::*;
 use itertools::izip;
 use std::iter;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let mut computer = Computer::load("17.txt")?;
     computer.mem[0] = 2;
 
-    let map: Vec<_> = computer.lines();
-    println!("{}\n", map.join("\n"));
+    let (tx, rx, _) = computer.channelled();
+    let mut ascii = Ascii::new(tx, rx);
 
-    let map: Vec<_> = map.iter().map(|line| line.as_bytes()).collect();
+    let map = ascii.paragraph().await.unwrap();
+    println!("{}\n", map);
+
+    let map: Vec<_> = map.lines().map(|line| line.as_bytes()).collect();
     let mut robot = Robot::load(&map);
     let moves: Vec<_> = iter::from_fn(|| robot.step(&map)).collect();
 
-    println!("{:?}", moves);
-    println!("{}", computer.line(&[])); // Main
+    println!("{:?}\n", moves);
+    println!("{}", ascii.line().await.unwrap()); // Main
 
     // Manually computed routines from moves
 
@@ -25,19 +30,15 @@ fn main() -> Result<()> {
         "R,10,L,12,L,12",
         "n",
     ] {
-        println!("{}", computer.line(&ascii(input)));
+        ascii.send(input).await?;
+        println!("{}", ascii.line().await.unwrap());
     }
 
-    let map: Vec<_> = computer.lines();
+    let map = ascii.paragraph().await.unwrap();
 
-    println!("{}\n", map.join("\n"));
-    println!("{:?}", computer.run(&[]));
-
+    println!("{}\n", map);
+    println!("{:?}", ascii.rx.collect::<Vec<_>>().await);
     Ok(())
-}
-
-fn ascii(s: &str) -> Vec<i64> {
-    s.bytes().map(|b| b as i64).chain(iter::once(10)).collect()
 }
 
 #[derive(Debug)]
@@ -99,94 +100,5 @@ impl Robot {
         }
 
         None
-    }
-}
-
-#[derive(Clone)]
-struct Computer {
-    base: usize,
-    mem: Vec<i64>,
-    ip: usize,
-}
-
-impl Computer {
-    fn load(filename: &str) -> Result<Self> {
-        let mut mem: Vec<i64> = input(filename)?
-            .split(',')
-            .map(|s| s.parse().unwrap())
-            .collect();
-
-        mem.resize(10 * 1024, 0);
-
-        Ok(Computer {
-            base: 0,
-            mem,
-            ip: 0,
-        })
-    }
-
-    fn lines(&mut self) -> Vec<String> {
-        iter::from_fn(|| Some(self.line(&[])).filter(|s| !s.is_empty())).collect()
-    }
-
-    fn line(&mut self, input: &[i64]) -> String {
-        iter::from_fn(|| self.run(input))
-            .map(|i| i as u8 as char)
-            .take_while(|&ch| ch != '\n')
-            .collect()
-    }
-
-    const OP_SIZE: &'static [usize] = &[0, 4, 4, 2, 2, 3, 3, 4, 4, 2];
-
-    fn run(&mut self, input: &[i64]) -> Option<i64> {
-        let mut input = input.iter();
-
-        loop {
-            let op = (self.mem[self.ip] % 100) as usize;
-
-            match op {
-                1 => *self.at(3) = *self.at(1) + *self.at(2),
-                2 => *self.at(3) = *self.at(1) * *self.at(2),
-                3 => *self.at(1) = *input.next().unwrap(),
-                4 => {
-                    let output = *self.at(1);
-                    self.ip += Self::OP_SIZE[op];
-                    return Some(output);
-                }
-                5 => {
-                    if *self.at(1) != 0 {
-                        self.ip = *self.at(2) as usize;
-                        continue;
-                    }
-                }
-                6 => {
-                    if *self.at(1) == 0 {
-                        self.ip = *self.at(2) as usize;
-                        continue;
-                    }
-                }
-                7 => *self.at(3) = if *self.at(1) < *self.at(2) { 1 } else { 0 },
-                8 => *self.at(3) = if *self.at(1) == *self.at(2) { 1 } else { 0 },
-                9 => self.base = (self.base as i64 + *self.at(1)) as usize,
-                99 => return None,
-                _ => unreachable!(),
-            }
-
-            self.ip += Self::OP_SIZE[op];
-        }
-    }
-
-    fn at(&mut self, parameter: usize) -> &mut i64 {
-        let mode = self.mem[self.ip] / 10i64.pow(parameter as u32 + 1) % 10;
-        let parameter = self.ip + parameter;
-
-        let address = match mode {
-            0 => self.mem[parameter] as usize,
-            1 => parameter,
-            2 => (self.base as i64 + self.mem[parameter]) as usize,
-            _ => unreachable!(),
-        };
-
-        &mut self.mem[address]
     }
 }
